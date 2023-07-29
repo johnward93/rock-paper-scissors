bb72751bcc9621af455716819c0c60971943bf93
John Ward <johnward127@gmail.com>
2023-07-29 12:29:59 +0100

    Add game() function for a first to 5 match
    Add a function which makes the match first to 5, displaying
    winner of each game in the console and keeping score
    A message is displayed on the console announcing
    the winner of the match

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
     let computerChoiceWord = ""
 
     if(computerChoiceNum === 0){
-        computerChoiceWord = "rock";
+        computerChoiceWord = "rock"
     }
     else if(computerChoiceNum === 1){
         computerChoiceWord = "paper"
@@ -20,23 +20,49 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     let result = ``
     if(!(playerSelection == "rock") && !(playerSelection == "paper") && !(playerSelection == "scissors")){
-        result = `${playerSelection} is not a valid option`
-        return result
+        console.log(`${playerSelection} is not a valid option`)
     }
     else if(playerSelection == computerSelection){
-        result = `It's a tie! You both chose the same option` 
+        result = `tie` 
         return result
     }
     else if((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")){
-        result = `You win! ${playerSelection} beats ${computerSelection}`
+        result = `player`
         return result
     }
     else {
-        result = `You lose! ${computerSelection} beats ${playerSelection}`
+        result = `computer`
         return result
     }
-  }
+}
 
-const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    let playerScore = 0
+    let computerScore = 0
+
+    while (playerScore < 5 && computerScore < 5){
+        const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors")
+        const computerSelection = getComputerChoice()
+        let winner = playRound(playerSelection, computerSelection)
+        if (winner === "tie"){
+            console.log(`It's a tie! You both chose ${playerSelection}`)
+            continue
+        }
+        else if (winner === "player"){
+            playerScore++
+            console.log(`You won that one! Player: ${playerScore} Computer: ${computerScore}`)
+        }
+        else if (winner === "computer"){
+            computerScore++
+            console.log(`You lost that one! Player: ${playerScore} Computer: ${computerScore}`)
+        }
+    }
+    if (playerScore === 5){
+        return `Player beat the computer`
+    }
+    else {
+        return `Computer must have cheated, it won`
+    }
+}
+
+console.log(game())
